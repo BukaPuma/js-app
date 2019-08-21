058c5a27ef2580d9f5c14ebcbacc3a1ad869ee59
BukaPuma <melvia@mail.ru>
2019-08-21 21:42:47 +0500

    add cicle

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 (function() {
     console.log('Hello git');
+    var items = ['one', 'two', 'three', 'four'];
+
+    for (var length = items.length, i = length-1; i >= 0; i--) {
+        console.log(items[i]);
+    }
 })()
